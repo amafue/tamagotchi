da3a5db8d99398de0b2b512766910f1e044dd3dd
amanda <amandafuentes831@gmail.com>
2026-09-07 21:42:44 +0200

    feat: add draw game function for game screen + file reading/writting

--- main.c
+++ main.c
@@ -1,14 +1,24 @@
 #define _XOPEN_SOURCE_EXTENDED 1
 #include <ncurses.h>
 #include <locale.h>
+#include <stdio.h>
 
 #define MENU_COUNT 3
 #define ACTION_COUNT 3
 #define STATE_MENU 0
 #define STATE_GAME 1
 
+char pet_name[50];
+int hunger;
+int happiness;
+int stress;
+
 int main(int argc, char **argv)
 {
+    void load_game(void);
+    void update_game(void);
+    void new_game(void);
+    void draw_game(char pet_name[], int hunger, int happiness, int stress);
 
     setlocale(LC_ALL, "");  //enables c to process nnon standard char
 
@@ -24,68 +34,10 @@ int main(int argc, char **argv)
     int input = 0;  //stores key int code returned by getch()
     int choice = -1;
 
-    const char *choices[] = {   //array of pointer addresses OJO
+    const char *choices[] = {   //array of pointer addresses
         "New Game",
         "Continue",
         "Quit"
-    };
-
-    bool is_happy = false;
-    bool is_hungry = false;
-    bool is_sleep = false;
-    bool is_stress = false;
-
-    char default_cat[][50] = {
-        "       /\\______/\\",
-        "     >| ˶ • ᴛ • ˶|< ",
-        "      /    𝇌♡𝇋   \\",
-    };
-
-    char hungry_cat[][50] = {
-        "       /\\______/\\",
-        "     >| ˶ o ᴛ o ˶|< ",
-        "      /    𝇌♡𝇋   \\",
-    };
-
-    char happy_cat[][50] = {
-        "       /\\______/\\",
-        "     >| ˶ > ᴛ < ˶|< ",
-        "      /    𝇌♡𝇋   \\",
-    };
-
-    char sleep_cat[][50] = {
-        "       /\\______/\\",
-        "     >|  〰 ᴛ 〰 |< ",
-        "      /    𝇌♡𝇋   \\",
-    };
-
-    char stress_cat[][50] = {
-        "       /\\______/\\",
-        "     >| ˶ @ ᴛ @ ˶|< ",
-        "      /    𝇌♡𝇋   \\",
-    };
-
-    char (*selected_cat)[50] = default_cat;   //pointer to the array
-
-    if (is_happy)
-    {
-        selected_cat = happy_cat;
-    } else if (is_hungry)
-    {
-        selected_cat = hungry_cat;
-    } else if (is_sleep)
-    {
-        selected_cat = sleep_cat;
-    } else if (is_stress)
-    {
-        selected_cat = stress_cat;
-    } else
-    {
-        selected_cat = default_cat;
-    }
-
-    for(int i = 0; i<3; i++){
-        printw("%s\n",selected_cat[i]);
     };
 
     while (1)
@@ -141,11 +93,13 @@ int main(int argc, char **argv)
                     return 0;
                 }else if (choice == 1)
                 {
+                    load_game();
                     state = STATE_GAME;
 
                 } else if (choice == 0)
                 {
-                    /* code */
+                    new_game();
+                    state = STATE_GAME;
                 }
                  
                 mvprintw(9,2,"You selected: %s\n", choices[choice]);
@@ -158,10 +112,7 @@ int main(int argc, char **argv)
 
         } else if (state == STATE_GAME)
         {
-            mvprintw(1,2,"Your pet:\n\n\n");
-            for(int i = 0; i<3; i++){
-                printw("%s\n",selected_cat[i]);
-            };
+            draw_game(pet_name, hunger, happiness, stress);
         }
         
         refresh();
@@ -175,5 +126,101 @@ int main(int argc, char **argv)
 
     endwin();    //leave ncurses mode
     return 0;
+
+}
+
+void load_game(void){
+    FILE *file = fopen("gameplay.txt", "r");
+
+    char text[150];
+    
+    fgets(text, 150, file);
+
+    fclose(file);
+
+
+}
+
+void save_game(void){
+
+    FILE *file = fopen("gameplay.txt", "w");
+
+    fprintf(file, "%s\n", pet_name);
+    fprintf(file, "%d\n", hunger);
+    fprintf(file, "%d\n", happiness);
+    fprintf(file, "%d\n", stress);
+
+    fclose(file);
+}
+
+void new_game(void){
+    int hunger = 100;
+    int happiness = 100;
+    int stress = 100;
+}
+
+void draw_game(char pet_name[], int hunger, int happiness, int stress){
+    bool is_happy = false;
+    bool is_hungry = false;
+    bool is_sleep = false;
+    bool is_stress = false;
+
+    char default_cat[][50] = {
+        "       /\\______/\\",
+        "     >| ˶ • ᴛ • ˶|< ",
+        "      /    𝇌♡𝇋   \\",
+    };
+
+    char hungry_cat[][50] = {
+        "       /\\______/\\",
+        "     >| ˶ o ᴛ o ˶|< ",
+        "      /    𝇌♡𝇋   \\",
+    };
+
+    char happy_cat[][50] = {
+        "       /\\______/\\",
+        "     >| ˶ > ᴛ < ˶|< ",
+        "      /    𝇌♡𝇋   \\",
+    };
+
+    char sleep_cat[][50] = {
+        "       /\\______/\\",
+        "     >|  〰 ᴛ 〰 |< ",
+        "      /    𝇌♡𝇋   \\",
+    };
+
+    char stress_cat[][50] = {
+        "       /\\______/\\",
+        "     >| ˶ @ ᴛ @ ˶|< ",
+        "      /    𝇌♡𝇋   \\",
+    };
+
+    char (*selected_cat)[50] = default_cat;   //pointer to the array
+
+    if (is_happy)
+    {
+        selected_cat = happy_cat;
+    } else if (is_hungry)
+    {
+        selected_cat = hungry_cat;
+    } else if (is_sleep)
+    {
+        selected_cat = sleep_cat;
+    } else if (is_stress)
+    {
+        selected_cat = stress_cat;
+    } else
+    {
+        selected_cat = default_cat;
+    }
+
+    for(int i = 0; i<3; i++){
+        mvprintw(i+3,28,"%s\n",selected_cat[i]);
+    };
+
+    mvprintw(1,5,"Name: %s", pet_name);
+    mvprintw(1,25,"Hunger: %d", hunger);
+    mvprintw(1,45,"Happiness: %d", happiness);
+    mvprintw(1,65,"Energi: %d", stress);
 
 }
